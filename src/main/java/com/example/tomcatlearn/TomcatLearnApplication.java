91ffddae94fe6847a934b857ffe1d750259a0e93
mutianjie <mutianjie@baidu.com>
2021-09-16 09:55:31 +0800

    init tomcat -learn

---
+++ src/main/java/com/example/tomcatlearn/TomcatLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomcatlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tomcatlearn/TomcatLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomcatlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
